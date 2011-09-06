ab3e712ff1b4417d80ce5322c4832caaf1f1ce57
Matthias Viehweger <kronn@kronn.de>
2011-09-06 17:10:08 +0200

    rename hoptoad to airbrake (thats how it is...)

---
+++ airbrake_to_pivotal.rb
@@ -0,0 +1,21 @@
+require "sinatra/base"
+require "./airbrake"
+require "./pivotal"
+
+class AirbrakeToPivotal < Sinatra::Base
+
+  $airbrake = Airbrake.new(ENV["AIRBRAKE_USERNAME"], ENV["AIRBRAKE_AUTH_TOKEN"], ENV["AIRBRAKE_PROJECT_ID"])
+
+  $pivotal = Pivotal.new(ENV["AIRBRAKE_REQUESTOR"])
+
+  use Rack::Auth::Basic do |username, password|
+    username == ENV["HTTP_BASIC_USERNAME"] && password == ENV["HTTP_BASIC_PASSWORD"]
+  end
+
+  get "/" do
+    bugs = $airbrake.bugs
+
+    $pivotal.bugs_to_xml(bugs)
+  end
+
+end
